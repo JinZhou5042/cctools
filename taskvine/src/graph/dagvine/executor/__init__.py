e91b470c14557e876c2eff49d80180973a7f0522
Jin Zhou <jzhou24@condorfe.crc.nd.edu>
2026-05-04 14:23:56 -0400

    fix cvine path

--- taskvine/src/graph/dagvine/executor/__init__.py
+++ taskvine/src/graph/dagvine/executor/__init__.py
@@ -2,6 +2,11 @@
 # This software is distributed under the GNU General Public License.
 # See the file COPYING for details.
 
+import importlib
+import sys
+
+# SWIG-generated graph_capi.py does "import cvine"; wire the top-level name to the real module.
+sys.modules.setdefault("cvine", importlib.import_module("ndcctools.taskvine.cvine"))
 
 from . import graph_capi
 from .graph import ExecutorGraph
